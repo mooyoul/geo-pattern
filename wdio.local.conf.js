b407152cd987bbd4cd967ff0b88090fa298e8faa
MooYeol Prescott Lee <mooyoul@gmail.com>
2020-05-25 04:29:51 +0900

    test(chromedriver): launch chrome as headless mode in CI env

--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -1,13 +1,18 @@
 'use strict';
 
+const { isCi } = require('env-ci')();
 const base = require('./wdio.base.conf');
+
+const chromeDriverOptions = isCi ? {
+  args: ['--headless', '--window-size=1024x768', '--disable-gpu'],
+} : {};
 
 exports.config = {
   ...base.config,
   logLevel: 'info',
   services: [
     ...base.config.services,
-    ['chromedriver'],
+    ['chromedriver', chromeDriverOptions],
   ],
   capabilities: [{
     'browserName': 'chrome',
